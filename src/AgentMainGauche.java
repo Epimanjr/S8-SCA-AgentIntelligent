e0e01dcc9b4e4389d4530490860ccaf6fd22068e
Maxime BLAISE <maxime.blaise12@gmail.com>
2016-04-10 15:40:36 +0000

    Ajout de l'algo de base

--- src/AgentMainGauche.java
+++ src/AgentMainGauche.java
@@ -1,11 +1,18 @@
 
 public class AgentMainGauche implements Agent {
 
-	
-
-	@Override
-	public Action decider(Observation o) {
-		throw new Error("A COMPLETER");
-	}
+    @Override
+    public Action decider(Observation o) {
+        if(o.murDevant) {
+            // Si un mur à droite, on va à gauche, sinon on va à droite
+            return (o.murDroite) ? Action.GAUCHE : Action.DROITE;
+        } else {
+            /*
+             Pour rémuser, on va tout le temps devant, 
+             Sauf si un mur à droite et pas à gauche
+            */
+            return (!o.murGauche && o.murDroite) ? Action.GAUCHE: Action.DEVANT;
+        }
+    }
 
 }
